642528167570cbcb0bd82aa285339806dfb23734
Ahmad Nazir <ahmadnazir9101@gmail.com>
2026-04-26 16:51:10 +0500

    All done before fiting the model

--- data/raw/notebooks/Model_Training.py
+++ data/raw/notebooks/Model_Training.py
@@ -1,7 +1,17 @@
 from exploration_and_peak_logic import train_scaled, test_scaled
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+import wandb
+from wandb.integration.keras import WandbCallback
 import numpy as np
-window_size=96
+import os
+from dotenv import load_dotenv
+load_dotenv() # This loads the variables from the .env file
+wandb.login(key=os.getenv("WANDB_API_KEY"))
 
+
+
+window_size=96
 def create_sequences(data,window_size):
     X,y=[],[]
 
@@ -17,3 +27,33 @@ X_test,y_test=create_sequences(test_scaled,window_size)
 #for testing purpose only
 print("X_train shape:", X_train.shape)
 print("y_train shape:", y_train.shape)
+
+
+model=tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])),
+    tf.keras.layers.LSTM(64,return_sequences=True),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.LSTM(32,return_sequences=False),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(16,activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+wandb.init(project="Industrial Power Optimizer",
+           name="LSTM_model",
+           config={
+               'lstm_units_1': 64,
+               'lstm_units_2': 32,
+               'dropout': 0.2,
+               'batch_size': 32,
+               'epochs': 50,
+               'window_size':96,
+               'optimizer': 'adam',
+                         }
+                         ,reinit=True)
+early_stop=EarlyStopping(monitor='val_loss',patience=5,restore_best_weights=True)
+wandb_callback = WandbCallback()
+
+
+model.compile(optimizer='adam',loss='mse',metrics=['mae','mape'])
+model.summary()
